98445063a789d46ed6faaa6b6872ffb135619903
inktomi <matthew.runo@gmail.com>
2012-12-12 17:58:54 -0800

    Moved the common code for  the two doors into an abstract class that both extend.

---
+++ src/com/ignition/apps/gangnam/Door.java
@@ -0,0 +1,80 @@
+package com.ignition.apps.gangnam;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLUtils;
+
+import javax.microedition.khronos.opengles.GL10;
+import java.nio.FloatBuffer;
+
+public abstract class Door {
+
+    abstract public FloatBuffer getVertexBuffer();
+    abstract public float[] getVertices();
+
+    protected FloatBuffer textureBuffer;	// buffer holding the texture coordinates
+    protected float[] texture = {
+            // Mapping coordinates for the vertices
+            0.0f, 1.0f,		// top left		(V2)
+            0.0f, 0.0f,		// bottom left	(V1)
+            1.0f, 1.0f,		// top right	(V4)
+            1.0f, 0.0f		// bottom right	(V3)
+    };
+
+    /** The texture pointer */
+    protected int[] textures = new int[1];
+
+    /** The draw method for the square with the GL context */
+    public void draw(GL10 gl) {
+        // bind the previously generated texture
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);
+
+        // Point to our buffers
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+
+        // Set the face rotation
+        gl.glFrontFace(GL10.GL_CW);
+
+        // Point to our vertex buffer
+        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, getVertexBuffer());
+        gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, textureBuffer);
+
+        // Draw the vertices as triangle strip
+        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, getVertices().length / 3);
+
+        //Disable the client state before leaving
+        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+    }
+
+    /**
+     * Load the texture for the door
+     */
+    public void loadGLTexture(GL10 gl, Context context, int drawableResId) {
+        // loading texture
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(),
+                drawableResId);
+
+        // generate one texture pointer
+        gl.glGenTextures(1, textures, 0);
+        // ...and bind it to our array
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);
+
+        // create nearest filtered texture
+        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_NEAREST);
+        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+
+        //Different possible texture parameters, e.g. GL10.GL_CLAMP_TO_EDGE
+//		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S, GL10.GL_REPEAT);
+//		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_REPEAT);
+
+        // Use Android GLUtils to specify a two-dimensional texture image from our bitmap
+        GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
+
+        // Clean up
+        bitmap.recycle();
+    }
+
+}
